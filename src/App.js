13c1faeff4287986ee324652dfc4aaac2d0b8105
Susan Liu <susanliu@Susans-Air.lan>
2023-01-28 17:25:01 -0500

    update index

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
-}
-
-function tick() {
-  root.render(<Clock date={new Date()} />);
-}
-
+ // not viewed
 
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    setInterval(tick, 1000);
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    
 
   );
 }
-
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,53 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    // interval call tick
+    return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  }
+}
+
+
+function tick() {
+  root.render(<Clock />);
+}
+
+setInterval(tick, 1000);
+// root.render(
+//   <React.StrictMode>
+    
+//     // <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
